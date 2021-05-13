89fad164c648099b99b1e2de640ba95a7e9d9f4d
nidhakal <49347059+nidhakal@users.noreply.github.com>
2021-05-13 14:29:37 -0400

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,12 +1,4 @@
-# utilities.py - AIPython useful utilities
-# AIFCA Python3 code Version 0.7.6 Documentation at http://aipython.org
 
-# Artificial Intelligence: Foundations of Computational Agents
-# http://artint.info
-# Copyright David L Poole and Alan K Mackworth 2017.
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# See: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
 
 import random
 
