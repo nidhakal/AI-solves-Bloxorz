708022c092286003e655281f2b9b75f3d1c12da7
nidhakal <49347059+nidhakal@users.noreply.github.com>
2021-05-13 14:29:08 -0400

    Update searchProblem.py

--- searchProblem.py
+++ searchProblem.py
@@ -1,12 +1,4 @@
-# searchProblem.py - representations of search problems
-# AIFCA Python3 code Version 0.7.6 Documentation at http://aipython.org
 
-# Artificial Intelligence: Foundations of Computational Agents
-# http://artint.info
-# Copyright David L Poole and Alan K Mackworth 2017.
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# See: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
 
 class Search_problem(object):
     """A search problem consists of:
